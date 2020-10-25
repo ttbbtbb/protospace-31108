07cefb26699b014d069a6b74c35934533fbefe2c
ttbbtbb <m86.tabata.masao@gmail.com>
2020-10-26 03:04:58 +0900

    アクションとルーティングを設定しましょう

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,12 @@
 class PrototypesController < ApplicationController
   def index
   end
+
+  def new
+  
+  end
+
+  def create
+  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :prototypes, only: [:index]
+  resources :prototypes, only: [:index, :new, :create]
   root to: "prototypes#index"
 end
