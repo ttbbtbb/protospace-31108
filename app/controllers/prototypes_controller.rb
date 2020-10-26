84e619b1a802542924a37d091abdee856fcd348c
ttbbtbb <m86.tabata.masao@gmail.com>
2020-10-27 06:57:35 +0900

    削除機能実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -34,8 +34,11 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
   end
-  
+
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
