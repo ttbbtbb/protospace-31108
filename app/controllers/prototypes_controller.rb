abe36df2a4d8ace9a3439390f0b62a1c0e79e9e2
ttbbtbb <m86.tabata.masao@gmail.com>
2020-10-26 15:49:10 +0900

    投稿機能を実装しましょう

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,10 +3,21 @@ class PrototypesController < ApplicationController
   end
 
   def new
-  
+    @prototype = Prototype.new
+  end
+    
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
-  def create
-  
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
+
 end
