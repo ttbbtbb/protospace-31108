e4dc96b2a967b509c0bbe5123ef60f09b390d62b
ttbbtbb <m86.tabata.masao@gmail.com>
2020-10-27 06:09:02 +0900

    編集機能実装；アクション、ルーティング、ビューファイル初期設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :prototypes, only: [:index, :new, :create, :show]
+  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update]
   root to: "prototypes#index"
 end
