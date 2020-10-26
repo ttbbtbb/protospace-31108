0fc4be8704d626b040c6a9df116bd301162acebb
ttbbtbb <m86.tabata.masao@gmail.com>
2020-10-26 17:51:30 +0900

    投稿したプロトタイプがトップページで表示されるようにしましょう

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,11 +1,12 @@
 class PrototypesController < ApplicationController
   def index
+    @prototypes = Prototype.all
   end
 
   def new
     @prototype = Prototype.new
   end
-    
+
   def create
     @prototype = Prototype.new(prototype_params)
     if @prototype.save
