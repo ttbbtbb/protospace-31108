45143027aadbd49a29782f36443f8517f4e0e9bc
ttbbtbb <m86.tabata.masao@gmail.com>
2020-10-27 21:12:47 +0900

    ユーザーによってアクセスできる範囲を制限しよう

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,6 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -24,6 +26,7 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    redirect_to action: :index unless current_user.id == @prototype.user_id
   end
 
   def update
