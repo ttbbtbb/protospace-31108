c2b24cc9b86fbe22b31b3c5838b43eacccb2d845
ttbbtbb <m86.tabata.masao@gmail.com>
2020-10-27 11:31:45 +0900

    コントローラーを作成し、アクションとルーティングを設定しましょう

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :prototypes
+  resources :prototypes do
+    resources :comments, only: [:create]
+  end
   root to: "prototypes#index"
 end
